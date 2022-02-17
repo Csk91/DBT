fdc7c30fb667685885ef342d79911925edb30548
Chandrashekar M <chandrashekar.muppala@aretove.com>
2022-02-17 19:05:37 +0000

    changes made in stg_payments

--- models/Staging/stg_payments.sql
+++ models/Staging/stg_payments.sql
@@ -1,11 +1,11 @@
 with payments as (
 
     select 
-    id,
-    orderid,
+    id as payment_id,
+    orderid as order_id,
     paymentmethod as payment_method,
     status,
-    amount,
+    (amount/100) as amount ,
     created,
     _batched_at
 
