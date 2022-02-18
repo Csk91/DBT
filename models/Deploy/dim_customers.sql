e0184982f065701ae4b362dc479e332f1e0019c9
Chandrashekar M <chandrashekar.muppala@aretove.com>
2022-02-18 06:46:18 +0000

    updated dim_customers

--- models/Deploy/dim_customers.sql
+++ models/Deploy/dim_customers.sql
@@ -1,4 +1,3 @@
-{{ config(materialized= "table") }}
 
 with customers as (
 
@@ -8,7 +7,7 @@ select * from {{ ref('stg_customers')}}
 
 orders as (
 
-select * from {{ ref('stg_orders') }}
+select * from {{ ref('fact_orders') }}
 
 ),
 
@@ -16,11 +15,10 @@ customer_orders as (
 
     select
         customer_id,
-
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
-
+        count(order_id) as number_of_orders,
+        sum(amount) as lifetime_value
     from orders
 
     group by 1
@@ -33,12 +31,11 @@ final as (
     select
         customers.customer_id,
         customers.full_name,
-        customer_orders.first_order_date,
-        customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
-
+        (case when customer_orders.first_order_date is null then '2050-12-31' else first_order_date end) as first_order_date,
+        (case when customer_orders.most_recent_order_date is null then '2050-12-31' else most_recent_order_date end) as most_recent_order_date,
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+        coalesce(customer_orders.lifetime_value,0) as lifetime_value
     from customers
-
     left join customer_orders using (customer_id)
 
 )
